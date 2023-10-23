8fa68e96df503138e8ea813a0eac32e610a2fdb1
Chuck Chen <chuck.j.chen@gmail.com>
2023-10-23 13:46:02 +0800

    Create chuckchen/tools tap

---
+++ Formula/sing-box.rb
@@ -0,0 +1,74 @@
+class SingBox < Formula
+  desc "Universal proxy platform"
+  homepage "https://sing-box.sagernet.org"
+  # using `:homebrew_curl` to work around audit failure from TLS 1.3-only homepage
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.5.3.tar.gz", using: :homebrew_curl
+  sha256 "e050f8a588d92547ba277fc8980af8ede544c345684c20e4b008fbc1b1371fb8"
+  license "GPL-3.0-or-later"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/sagernet/sing-box/constant.Version=#{version} -buildid="
+    tags = "with_gvisor,with_quic,with_wireguard,with_utls,with_reality_server,with_clash_api"
+    system "go", "build", "-tags", tags, *std_go_args(ldflags: ldflags), "./cmd/sing-box"
+  end
+
+  service do
+    run [opt_bin/"sing-box", "run", "--config", "#{etc}/sing-box/config.json"]
+    run_type :immediate
+    keep_alive true
+  end
+
+  test do
+    ss_port = free_port
+    (testpath/"shadowsocks.json").write <<~EOS
+      {
+        "inbounds": [
+          {
+            "type": "shadowsocks",
+            "listen": "::",
+            "listen_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    EOS
+    server = fork { exec "#{bin}/sing-box", "run", "-D", testpath, "-c", testpath/"shadowsocks.json" }
+
+    sing_box_port = free_port
+    (testpath/"config.json").write <<~EOS
+      {
+        "inbounds": [
+          {
+            "type": "mixed",
+            "listen": "::",
+            "listen_port": #{sing_box_port}
+          }
+        ],
+        "outbounds": [
+          {
+            "type": "shadowsocks",
+            "server": "127.0.0.1",
+            "server_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    EOS
+    system "#{bin}/sing-box", "check", "-D", testpath, "-c", "config.json"
+    client = fork { exec "#{bin}/sing-box", "run", "-D", testpath, "-c", "config.json" }
+
+    sleep 3
+    begin
+      system "curl", "--socks5", "127.0.0.1:#{sing_box_port}", "github.com"
+    ensure
+      Process.kill 9, server
+      Process.wait server
+      Process.kill 9, client
+      Process.wait client
+    end
+  end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# Some Randoms Tools
+
+## Why this tap?
+
+Homebrew is a very cool package manager. But occasionally I found some packages are simply not what I need. E.g. missing service scripts, tool old versions. This tap holds better versions of those packages until those are fixed in the core tap.
+
+## How do I install these formulae?
+
+`brew install chuckchen/tools/<formula>`
+
+Or `brew tap chuckchen/tools` and then `brew install <formula>`.
+
